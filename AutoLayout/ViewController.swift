f24cd74cb74196b5fdd6a522a8937581bcf3805c
chiaqingwu <secret8419@gmail.com>
2020-07-27 22:24:34 +0800

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by chiaqingwu on 2020/7/27.
+//  Copyright © 2020 Andrewwuuw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
